ab8ed23305044d30e0ffcb7e83ca5e07dfc60b30
SonnieCodes <mgathoni.gathoni9@gmail.com>
2024-12-21 09:57:35 +0300

    extends

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Your code here
+class Polygon{
+    constructor(Array){
+        this.Array = Array;
+    }
+    get countSides(){
+        return this.Array.length;
+    }
+    get perimeter(){
+        for(let i in this.Array){
+            return this.Array.reduce((acc, Array)=> acc + Array, 0);
+        }
+    }
+}
+
+class Triangle extends Polygon{
+    get isValid(){
+        if(this.countSides !== 3){
+            return false;
+        }
+        const [x,y,z] = this.Array;
+        return x+y>z &&y+z>x && x+z>y;
+    }
+}
+class Square extends Polygon{
+    get isValid(){
+        if (this.countSides !== 4){
+            return false;
+        }
+        return this.Array.every(arr => arr === this.Array[0]);
+    }
+
+    get area(){
+        if(!this.isValid){
+            return undefined;
+        }
+        return this.Array[0] ** 2;
+    }
+}
